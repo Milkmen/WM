e605bc1c1906f47e309a17383f7294e16bfb6555
Milkman <ne_flohr@protonmail.com>
2026-01-28 22:45:52 +0100

    fix7

--- test/apt.sh
+++ test/apt.sh
@@ -1,4 +1,4 @@
-sudo apt install xorg libx11-dev libxft-dev libxrender-dev libxinerama-dev libxrandr-dev xorg-apps -y
+sudo apt install xorg libx11-dev libxft-dev libxrender-dev libxinerama-dev libxrandr-dev x11-apps -y
 make -C ..
 cp .xinitrc ~
 startx
